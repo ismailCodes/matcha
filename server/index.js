ef6424feff064b00da4f4ff5336a76ab5da3cc91
Anas Zineddine <azineddi@e1r2p3.1337.ma>
2021-02-23 10:12:58 +0100

    Refactor code

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+//const cors = require("cors");
 
 app.listen(5000, () => {
     console.log("server is running on port 5000");
