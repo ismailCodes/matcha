15a65b5e098b736a306d69333d2cafcdb360fc0d
r3tard3dd <i.iem@bk.ru>
2021-03-04 17:48:57 +0100

    EOD commit

--- Client/src/components/Layout.jsx
+++ Client/src/components/Layout.jsx
@@ -7,62 +7,65 @@ import {
   faCog,
 } from '@fortawesome/free-solid-svg-icons';
 
-function Layout(/*Component, props*/) {
+function Layout({ children }) {
   return (
-    <header className='text-gray-200 body-font bg-indigo-700 w-full'>
-      <div className='flex p-2 md:p-4 flex-row items-center w-full justify-between'>
-        <div className='flex w-1/3 sm:w-auto justify-center title-font font-medium items-center text-gray-200 sm:mr-8 px-3 order-2 sm:order-1'>
-          <FontAwesomeIcon size='2x' icon={faMortarPestle} />
-          <span className='ml-3 text-xl hidden sm:block'>MATCHA</span>
-        </div>
-        <div className='md:ml-auto w-1/3 sm:w-auto flex flex-wrap justify-end items-center text-base sm:justify-center order-3 sm:order-2 sm:mr-3'>
-          <FontAwesomeIcon
-            className='mx-3 hover:text-gray-300'
-            size='2x'
-            icon={faEnvelope}
-          />
-          <FontAwesomeIcon
-            className='mx-3 hover:text-gray-300'
-            size='2x'
-            icon={faBell}
-          />
-        </div>
-        <div className='flex w-1/3 sm:w-auto flex-row items-center order-1 sm:order-3'>
-          <div className='group inline-block '>
-            <div className='w-12 h-12 flex items-center'>
-              <img
-                className='rounded-full border hover:border-red-500 border-gray-100 shadow-sm hidden sm:block'
-                src='https://randomuser.me/api/portraits/women/81.jpg'
-                alt='user image'
-              />
-              <FontAwesomeIcon
-                className='mx-3 hover:text-gray-300 block sm:hidden'
-                size='2x'
-                icon={faCog}
-              />
-            </div>
-            <ul className='absolute hidden text-gray-700 pt-2 group-hover:block sm:-ml-12'>
-              <li className=''>
-                <a
-                  className='rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
-                  href='#'
-                >
-                  Profile
-                </a>
-              </li>
-              <li className=''>
-                <a
-                  className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
-                  href='#'
-                >
-                  Sign Out
-                </a>
-              </li>
-            </ul>
+    <div className='w-full h-full'>
+      <header className='text-gray-200 body-font bg-red-500 w-full'>
+        <nav className='flex p-1 md:p-2 flex-row items-center w-full justify-between'>
+          <div className='flex w-1/3 sm:w-auto justify-center title-font font-medium items-center text-gray-200 sm:mr-8 px-3 order-2 sm:order-1'>
+            <FontAwesomeIcon size='2x' icon={faMortarPestle} />
+            <span className='ml-3 text-xl hidden sm:block'>MATCHA</span>
           </div>
-        </div>
-      </div>
-    </header>
+          <div className='md:ml-auto w-1/3 sm:w-auto flex flex-wrap justify-end items-center text-base sm:justify-center order-3 sm:order-2 sm:mr-3'>
+            <FontAwesomeIcon
+              className='mx-3 hover:text-gray-300'
+              size='2x'
+              icon={faEnvelope}
+            />
+            <FontAwesomeIcon
+              className='mx-3 hover:text-gray-300'
+              size='2x'
+              icon={faBell}
+            />
+          </div>
+          <div className='flex w-1/3 sm:w-auto flex-row items-center order-1 sm:order-3'>
+            <div className='group inline-block '>
+              <div className='w-12 h-12 flex items-center'>
+                <img
+                  className='rounded-full border hover:border-red-500 border-gray-100 shadow-sm hidden sm:block'
+                  src='https://randomuser.me/api/portraits/women/81.jpg'
+                  alt='user image'
+                />
+                <FontAwesomeIcon
+                  className='mx-3 hover:text-gray-300 block sm:hidden'
+                  size='2x'
+                  icon={faCog}
+                />
+              </div>
+              <ul className='absolute hidden text-gray-700 pt-2 group-hover:block sm:-ml-12'>
+                <li className=''>
+                  <a
+                    className='rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
+                    href='#'
+                  >
+                    Profile
+                  </a>
+                </li>
+                <li className=''>
+                  <a
+                    className='rounded-b bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap'
+                    href='#'
+                  >
+                    Sign Out
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </header>
+      {children}
+    </div>
   );
 }
 
--- Client/src/pages/Profile.jsx
+++ Client/src/pages/Profile.jsx
@@ -2,10 +2,23 @@ import Layout from '../components/Layout';
 
 function Profile() {
   return (
-    <div className='w-full h-full'>
-      <Layout /*Component={Something} pageProps={Something}*/ />
-    </div>
+    <Layout>
+      <div className='w-full h-full items-center flex flex-col sm:pt-4'>
+        <div className='w-full sm:max-w-md bg-gray-600 h-96 relative'>
+          <button className='rounded-full border bg-red-300 absolute bottom-0 left-4 transform translate-y-1/2 h-20 w-20'>
+            TZ
+          </button>
+        </div>
+        <div className='self-start pt-12 px-6 font-bold text-2xl'>
+          Laarbi LHILALI
+        </div>
+      </div>
+    </Layout>
   );
 }
 
 export default Profile;
+
+<comp>
+  <comp2></comp2>
+</comp>;
