cc315351a7bd6451c407c2092f32684335c0a3b9
Anas Zineddine <azineddi@e1r6p10.1337.ma>
2021-03-19 17:01:28 +0100

    add reset password to test

--- Server/graphql/resolvers/users.js
+++ Server/graphql/resolvers/users.js
@@ -2,13 +2,17 @@ const bcrypt = require("bcryptjs");
 require("dotenv").config();
 const { UserInputError } = require("apollo-server");
 const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const pool = require("../../db");
 const {
   validateRegisterInput,
   validateLoginInput,
 } = require("../../util/validators");
-const { sendConfirmationEmail } = require("../../services/emailService");
+const {
+  sendConfirmationEmail,
+  sendResetEmail,
+} = require("../../services/emailService");
 const { generateToken } = require("../../util/generateToken");
 
 module.exports = {
@@ -32,6 +36,10 @@ module.exports = {
       if (!validPassword) {
         errors.general = "Wrong credentials";
         throw new UserInputError("Wrong credentials", { errors });
+      }
+      if (!user.rows[0].is_verified) {
+        errors.general = "User is not verified";
+        throw new UserInputError("User is not verified", { errors });
       }
 
       const token = generateToken(user);
@@ -106,12 +114,76 @@ module.exports = {
     async confirmEmail(_, { token }) {
       try {
         const verifyToken = jwt.verify(token, process.env.jwtSecret);
-        console.log(verifyToken);
+        await pool.query(
+          "UPDATE users SET is_verified = 'true' WHERE user_id = $1",
+          [verifyToken.id]
+        );
+        //console.log(verifyToken);
         return true;
       } catch (error) {
         console.log(error);
         return false;
       }
+    },
+    async recoverPassword(_, { email }) {
+      //TODO:check email is valid ??
+
+      const user = await pool.query(
+        "SELECT * FROM users WHERE user_email = $1",
+        [email]
+      );
+      if (user.rows.length === 0) {
+        throw new UserInputError(
+          "The email address is not associated with any account",
+          {
+            errors: {
+              email: "The email address is not associated with any account", // for frontend ??
+            },
+          }
+        );
+      }
+      const resetToken = await crypto.randomBytes(20).toString("hex");
+      const resetTokenExpiry = Date.now() + 3600000;
+      const user_email = await pool.query(
+        "UPDATE users SET reset_password_token = $1 ,reset_password_expiry = $2 WHERE user_email = $3 RETURNING *",
+        [resetToken, resetTokenExpiry.toString(), email]
+      ); // Change RETURNING * ?? to only return what needed ??
+      sendResetEmail(user_email);
+
+      //console.log(resetToken);
+      //console.log(resetTokenExpiry);
+      //console.log(user.rows[0].user_email);
+      return { email };
+    },
+    async resetPassword(
+      _,
+      { resetInput: { password, confirmPassword, resetToken } },
+      context,
+      info
+    ) {
+      //TODO: validate input agaaaaaaaain and throw Userinputerror of apollo
+      if (password !== confirmPassword) {
+        throw new Error(`Your passwords don't match`);
+      }
+      const user = await pool.query(
+        "SELECT * FROM users WHERE reset_password_token = $1",
+        [resetToken]
+      );
+      if (user.rows.length === 0 /* || TODO: check expiracy date here ??*/) {
+        //errors.general =
+        //"Your password reset token is either invalid or expired.";
+        throw new UserInputError(
+          "Your password reset token is either invalid or expired."
+        );
+      }
+      const newBcryptPassword = await bcrypt.hash(password, 12);
+      const user_email = await pool.query(
+        "UPDATE users SET user_password = $1 WHERE reset_password_token = $2 RETURNING *",
+        [newBcryptPassword, resetToken]
+      );
+      // Send email of notif change email ??
+      return { email: user.rows[0].user_email }; // not sure yet what to return to client...
+      //TODO: check again all of this
     },
   },
 };
--- Server/services/emailService.js
+++ Server/services/emailService.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 const { generateToken } = require("../util/generateToken");
 
-var transporter = nodemailer.createTransport({
+// Gmail transporter
+
+/*var transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
@@ -11,17 +13,46 @@ var transporter = nodemailer.createTransport({
     user: `${process.env.email}`,
     pass: `${process.env.password}`,
   },
+});*/
+
+// http://ethereal.email/ Transporter
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: `${process.env.email}`,
+    pass: `${process.env.password}`,
+  },
 });
 
 const sendConfirmationEmail = (user) => {
   const token = generateToken(user);
-  const url = `http://localhost:5000/confirmation/${token}`;
+  const url = `http://localhost:3000/confirmation/${token}`;
 
   transporter.sendMail({
     from: `${process.env.email}`,
     to: `${user.rows[0].user_first_name} <${user.rows[0].user_email}>`,
-    subject: "hello world",
-    html: `Confirmation email <a href=${url}> ${url}</a>`,
+    subject: "Confirmation email",
+    html: `Confirmation link <a href=${url}> ${url}</a>`,
+  }),
+    function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("email sent : " + info.response);
+      }
+    };
+};
+
+const sendResetEmail = (user) => {
+  const url = `http://localhost:3000/change-password/${user.rows[0].reset_password_token}`;
+
+  transporter.sendMail({
+    from: `${process.env.email}`,
+    to: `${user.rows[0].user_first_name} <${user.rows[0].user_email}>`,
+    subject: "Reset password",
+    html: `Reset password link <a href=${url}> ${url}</a>`,
   }),
     function (error, info) {
       if (error) {
@@ -33,3 +64,4 @@ const sendConfirmationEmail = (user) => {
 };
 
 exports.sendConfirmationEmail = sendConfirmationEmail;
+exports.sendResetEmail = sendResetEmail;
